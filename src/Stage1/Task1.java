5f34ea4c5605d297d03e32f45c50ab5edd4cd114
Oksana Davydovich <ks.davydovich@gmail.com>
2019-06-03 10:09:28 +0300

    Stage1 Task1 done

---
+++ src/Stage1/Task1.java
@@ -0,0 +1,21 @@
+package Stage1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input number a:");
+        int a = scanner.nextInt();
+
+        System.out.print("Input number b:");
+        int b = scanner.nextInt();
+
+        System.out.print("Input number с:");
+        int c = scanner.nextInt();
+
+        double z = ((a - 3) * b / 2) + c;
+        System.out.print("z = " + z);
+
+    }
+}
